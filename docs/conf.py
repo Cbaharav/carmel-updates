ed51111f1bd61ae1831bc90fb5e2f31e12c99361
Carmel Baharav <carmel.baharav@gmail.com>
2018-06-07 11:29:54 -0700

    new method of import extensions

--- docs/conf.py
+++ docs/conf.py
@@ -47,11 +47,13 @@ import sphinx_rtd_theme
 
 # MOCK_MODULES = ['IPython.sphinxext.ipython_console_highlighting', 'IPython.sphinxext.ipython_directive']
 # sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+import sys, os
+sys.path.append(os.path.abspath('IPython.sphinxext'))
 
 extensions = ['sphinx.ext.viewcode', 
               'IPython.sphinxext.ipython_console_highlighting',
               'IPython.sphinxext.ipython_directive']
-              
+
 # ipython_mplbackend = None
 # Sphinx.add_directive('ipython', IPythonDirective)               
 
